2c84deab8a8d05c33530b63a59a00a3edc30e4d4
Patrick Huston <patrickhuston1@gmail.com>
2015-04-21 22:31:37 -0400

    Adds high pass filter functionality

--- imagecompress.py
+++ imagecompress.py
@@ -1,31 +1,45 @@
-import cv2
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('img/img.jpg',0)
-f = np.fft.fft2(img)
-fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
 
 
-rows, cols = img.shape
-crow,ccol = rows/2 , cols/2
-fshift[crow-30:crow+30, ccol-30:ccol+30] = 0
-f_ishift = np.fft.ifftshift(fshift)
-img_back = np.fft.ifft2(f_ishift)
-img_back = np.abs(img_back)
+# Convert image to np array, shift DC to center of image
+dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT) 
+dft_shift = np.fft.fftshift(dft)
 
-plt.subplot(131),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
-plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
-plt.subplot(133),plt.imshow(img_back)
-plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
-plt.show()
 
+# Displays frequency domain view of image - DC centered
 # plt.subplot(121),plt.imshow(img, cmap = 'gray')
 # plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 # plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
 # plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
 # plt.show()
+
+rows, cols = img.shape
+crow,ccol = rows/2 , cols/2
+
+# create a mask first, center square is 1, remaining all zeros
+maskLPF = np.zeros((rows,cols,2),np.uint8)
+maskLPF[crow-50:crow+50, ccol-50:ccol+50] = 1 # LPF - creates box of 1's in center to 'mask' input
+
+# create a mask first, center square is 0, remaining all ones
+maskHPF = np.ones((rows,cols,2),np.uint8)
+maskHPF[crow-50:crow+50, ccol-50:ccol+50] = 0 # HPF - creates box of 0's in center to 'mask' input
+
+
+
+# apply mask and inverse DFT
+fshift = dft_shift*maskHPF
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv2.idft(f_ishift)
+img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
