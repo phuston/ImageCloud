c7e10f53e1670a8ddcd5d81c2b36f30e26549ed9
Patrick Huston <patrickhuston1@gmail.com>
2015-04-24 11:11:36 -0400

    Removes extra unnecessary files

--- imagecompress.py
+++ imagecompress.py
@@ -16,11 +16,11 @@ magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
 
 # # Displays frequency domain view of image - DC centered
-# plt.subplot(121),plt.imshow(img, cmap = 'gray')
-# plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-# plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-# plt.show()
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
 
 rows, cols = img.shape
 img_size = np.array([rows,cols])
