836b3739f49a25a2a4fe2613bac250fafcd5c477
Keenan Zucker <Keenan.Zucker@students.olin.edu>
2015-04-24 11:16:20 -0400

    added images and graphingarray

--- imagecompress.py
+++ imagecompress.py
@@ -2,11 +2,10 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from scipy.io.wavfile import write
-
 # class Im2Audio():
 	
 
-img = cv2.imread('img/md1.jpg',0)
+img = cv2.imread('img/hStripesNew.png',0)
 
 # Convert image to np array, shift DC to center of image
 dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT) 
@@ -15,12 +14,12 @@ dft_shift = np.fft.fftshift(dft)
 magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
 
-# # Displays frequency domain view of image - DC centered
-# plt.subplot(121),plt.imshow(img, cmap = 'gray')
-# plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-# plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-# plt.show()
+# Displays frequency domain view of image - DC centered
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
 
 rows, cols = img.shape
 img_size = np.array([rows,cols])
@@ -44,8 +43,21 @@ arr_max = np.max(reshape_arr)
 scalar = 32767
 scaled_arr = np.array(reshape_arr/np.abs(arr_max) * scalar)
 
-
 full_sample = np.concatenate([img_size, np.array([arr_max,scalar]), scaled_arr])
+print full_sample
+
+total = 0
+for i in full_sample:
+	total += i
+averageAmp = total / len(full_sample)
+#print averageAmp
+
+maximum = np.amax(full_sample)
+
+plt.plot(full_sample)
+plt.axis([0,len(full_sample), -averageAmp * 100, averageAmp * 100] )
+#plt.show()
+
 
 write('test.wav', 44100, full_sample)
 
