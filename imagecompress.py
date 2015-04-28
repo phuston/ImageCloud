7cdf19273a98f3fbe2d0a0718ca7ffe32363f814
Keenan Zucker <Keenan.Zucker@students.olin.edu>
2015-04-27 21:05:58 -0400

    added plot of sound for poster

--- imagecompress.py
+++ imagecompress.py
@@ -54,15 +54,21 @@ class Im2Audio():
     def write(self, filename):
         write(filename, 44100, self.full_sample)
 
-    def plotWave(self):
+    def plot_wave(self):
 
     	total = 0
     	for i in self.full_sample:
     		total += i
 
     	av = total / self.full_sample.size
-    	print av
 
     	plt.plot(self.full_sample)
-    	plt.axis([0, self.full_sample.size, -av * 50, av * 50])
+    	plt.axis([0, self.full_sample.size, -av * 10, av * 10])
     	plt.show()
+
+
+#Generate Plots of Picture as Sound Wave
+
+#image = Im2Audio('img/img.jpg') 
+#image.shape_and_scale()
+#image.plot_wave()
